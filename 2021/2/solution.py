effb62bab8cb0ff79acc441b924e2b931d5ba95b
Isaac Oldwood <Isaac@mulberryrisk.com>
2022-10-26 14:43:57 +0100

    Solve 2021-2 part 2

--- 2021/2/solution.py
+++ 2021/2/solution.py
@@ -14,4 +14,16 @@ df.loc[df['direction'] == 'up', 'magnitude'] = df.loc[df['direction'] == 'up', '
 up_down_total = df.loc[df['direction'].isin(['up', 'down']), 'magnitude'].sum()
 forward_total = df.loc[df['direction'] == 'forward', 'magnitude'].sum()
 
-print(up_down_total * forward_total)
+# print(up_down_total * forward_total)
+
+# Part 2
+df['aim'] = df.loc[df['direction'].isin(['up', 'down']), 'magnitude'].cumsum()
+df['aim'].ffill(inplace=True)
+df.loc[0, 'aim'] = 0
+
+df['depth'] = df.loc[df['direction'] == 'forward', 'magnitude'] * df.loc[df['direction'] == 'forward', 'aim']
+df['depth'].fillna(0, inplace=True)
+
+depth_total = df['depth'].astype(int).sum()
+
+# print(forward_total * depth_total)
