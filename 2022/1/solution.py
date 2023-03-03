f34a9e126f5b9f45a25427bcd27fdf7a50749697
Isaac Oldwood <Isaac@mulberryrisk.com>
2023-03-03 15:57:49 +0000

    Solve 2022-1 part 2

--- 2022/1/solution.py
+++ 2022/1/solution.py
@@ -12,9 +12,33 @@ for cal in lines:
     if cal == '':
         if current_elf_calorie > highest_calorie:
             highest_calorie = current_elf_calorie
-        
         current_elf_calorie = 0
     else:
         current_elf_calorie += int(cal)
 
 print(highest_calorie)
+
+# Part 2
+with open(dir_path + '/input.txt') as file:
+    lines = [line.rstrip() for line in file]
+
+highest_calorie = 0
+second_highest_calorie = 0
+third_highest_calorie = 0
+current_elf_calorie = 0
+for cal in lines:
+    if cal == '':
+        if current_elf_calorie > highest_calorie:
+            third_highest_calorie = second_highest_calorie
+            second_highest_calorie = highest_calorie
+            highest_calorie = current_elf_calorie
+        elif current_elf_calorie > second_highest_calorie:
+            third_highest_calorie = second_highest_calorie
+            second_highest_calorie = current_elf_calorie
+        elif current_elf_calorie > third_highest_calorie:
+            third_highest_calorie = current_elf_calorie
+        current_elf_calorie = 0
+    else:
+        current_elf_calorie += int(cal)
+
+print(highest_calorie + second_highest_calorie + third_highest_calorie)
