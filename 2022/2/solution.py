91589f354195afc1ce39ea745e67ab9232728492
Isaac Oldwood <Isaac@mulberryrisk.com>
2023-04-03 09:53:29 +0100

    Solve 2022-2 part 2

--- 2022/2/solution.py
+++ 2022/2/solution.py
@@ -31,3 +31,50 @@ df.loc[
 
 total = df[['score_1', 'score_2']].sum().sum()
 print(total)
+
+# Part 2
+df = pd.read_fwf(dir_path + '/input.txt', header=None, dtype=str)
+df.rename(columns={0:'op',1:'end'}, inplace=True)
+df['me'] = 'Rock'
+
+df.replace('A', 'Rock', inplace=True)
+df.replace('B', 'Paper', inplace=True)
+df.replace('C', 'Scissors', inplace=True)
+
+# X
+rock_lose = (df['end'] == 'X') & (df['op'] == 'Rock')
+df.loc[rock_lose, 'me'] = 'Scissors'
+paper_lose = (df['end'] == 'X') & (df['op'] == 'Paper')
+df.loc[paper_lose, 'me'] = 'Rock'
+scissors_lose = (df['end'] == 'X') & (df['op'] == 'Scissors')
+df.loc[scissors_lose, 'me'] = 'Paper'
+
+# Y
+df.loc[df['end'] == 'Y', 'me'] = df.loc[df['end'] == 'Y', 'op']
+
+# Z
+rock_win = (df['end'] == 'Z') & (df['op'] == 'Rock')
+df.loc[rock_win, 'me'] = 'Paper'
+paper_win = (df['end'] == 'Z') & (df['op'] == 'Paper')
+df.loc[paper_win, 'me'] = 'Scissors'
+scissors_win = (df['end'] == 'Z') & (df['op'] == 'Scissors')
+df.loc[scissors_win, 'me'] = 'Rock'
+
+# Calc score
+df['score_1'] = 1
+df.loc[df['me'] == 'Paper','score_1'] = 2
+df.loc[df['me'] == 'Scissors','score_1'] = 3
+
+# Lost
+df['score_2'] = 0
+# Draw
+df.loc[df['me'] == df['op'],'score_2'] = 3
+# Win
+df.loc[
+    ((df['me'] == 'Rock') & (df['op'] == 'Scissors')) |
+    ((df['me'] == 'Paper') & (df['op'] == 'Rock')) |
+    ((df['me'] == 'Scissors') & (df['op'] == 'Paper'))
+    ,'score_2'] = 6
+
+total = df[['score_1', 'score_2']].sum().sum()
+print(total)
