d05b303c5e811e7eb88ec767a45b3c8be0fbdffa
Isaac Oldwood <Isaac@mulberryrisk.com>
2023-04-03 13:20:55 +0100

    Solve 2022-4 part 1

---
+++ 2022/4/solution.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Part 1
+df = pd.read_fwf(dir_path + '/input.txt', header=None, dtype=str)
+df.rename(columns={0:'ranges'}, inplace=True)
+
+df[['range1', 'range2']] = df['ranges'].str.split(',', expand=True)
+df[['range1_start', 'range1_end']] = df['range1'].str.split('-', expand=True)
+df[['range2_start', 'range2_end']] = df['range2'].str.split('-', expand=True)
+
+df['range1_set'] = [set(range(int(i), int(j)+1)) for i, j in df[['range1_start', 'range1_end']].values]
+df['range2_set'] = [set(range(int(i), int(j)+1)) for i, j in df[['range2_start', 'range2_end']].values]
+
+df['subset_1'] = df[['range1_set', 'range2_set']].apply(lambda x: set(x["range1_set"]).issubset(set(x["range2_set"])), axis=1)
+df['subset_2'] = df[['range1_set', 'range2_set']].apply(lambda x: set(x["range2_set"]).issubset(set(x["range1_set"])), axis=1)
+
+subsets = df[['subset_1', 'subset_2']].value_counts()
+
+print(subsets.sum() - (subsets.loc[subsets.index == (False, False)]).sum())
