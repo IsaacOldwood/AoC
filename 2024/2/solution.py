ac905417fdee2550bb351d478176be2982b496b9
Isaac Oldwood <isaac.oldwood@gmail.com>
2024-12-07 20:33:40 +0000

    Refactor 2024-2 part 1

--- 2024/2/solution.py
+++ 2024/2/solution.py
@@ -6,30 +6,24 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(dir_path + "/input.txt") as file:
     reports = [line.rstrip() for line in file]
 
-safe_reports = 0
-for report in reports:
-    # Split report into list of levels
-    levels = [int(x) for x in report.split()]
-    current_level = levels[0]
 
+def is_report_safe(levels: list[int]) -> bool:
+    current_level = levels[0]
     decreasing = False
     increasing = False
-    report_safe = True
 
     for n in range(1, len(levels)):
         next_level = levels[n]
 
         # Must increase or decrease by 1
         if next_level == current_level:
-            report_safe = False
-            break
+            return False
 
         diff = next_level - current_level
 
         # Must increase or decrease by less than 4
         if abs(diff) > 3:
-            report_safe = False
-            break
+            return False
 
         # Must be all increasing or all decreasing
         if diff > 0:
@@ -38,14 +32,24 @@ for report in reports:
             decreasing = True
 
         if increasing and decreasing:
-            report_safe = False
-            break
+            return False
 
         # Move to next level
         current_level = next_level
+
+    return True
+
+
+safe_reports = 0
+for report in reports:
+    # Split report into list of levels
+    levels = [int(x) for x in report.split()]
+
+    report_safe = is_report_safe(levels)
 
     # If all conditions are met, the report is safe
     if report_safe:
         safe_reports += 1
 
-print(f"Part 1: {safe_reports=}")
+print(f"Part 1: {safe_reports=}")  # 686
+print(f"Part 2: {safe_reports=}")  # not 714
