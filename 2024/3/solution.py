c68655b8e7da4b5b539ca4ae75301a4a25be0023
Isaac Oldwood <isaac.oldwood@gmail.com>
2024-12-07 21:13:30 +0000

    Solve 2024-3 part 2

--- 2024/3/solution.py
+++ 2024/3/solution.py
@@ -9,18 +9,28 @@ with open(dir_path + "/input.txt") as file:
 corrupted_memory = "".join(lines)
 
 # Find valid instructions
-instructions = re.findall(r"mul\(\d{1,3},\d{1,3}\)", corrupted_memory)
+instructions = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", corrupted_memory)
 
 total = 0
+mul_enabled = True
 # Clean and convert to pairs of numbers
 for instruction in instructions:
-    # Remove everything other than numbers
-    instruction = instruction.replace("mul(", "").replace(")", "")
+    if instruction == "do()":
+        mul_enabled = True
+        continue
+    elif instruction == "don't()":
+        mul_enabled = False
+        continue
 
-    # Split LHS and RHS
-    numbers = instruction.split(",")
+    if mul_enabled:
+        # Remove everything other than numbers
+        instruction = instruction.replace("mul(", "").replace(")", "")
 
-    # Convert to integers and multiply
-    total += int(numbers[0]) * int(numbers[1])
+        # Split LHS and RHS
+        numbers = instruction.split(",")
 
-print(f"Part 1: {total=}")
+        # Convert to integers and multiply
+        total += int(numbers[0]) * int(numbers[1])
+
+print(f"Part 1: {total=}")  # 189527826
+print(f"Part 2: {total=}")  # 63013756
