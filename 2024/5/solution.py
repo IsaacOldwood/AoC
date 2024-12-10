dd378d960e86421432320e36035046a79c38cb7e
Isaac Oldwood <isaac.oldwood@gmail.com>
2024-12-10 10:50:46 +0000

    Solve 2024-5 part 2

--- 2024/5/solution.py
+++ 2024/5/solution.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Rules are stored as a dictionary to easily find all page numbers a given page needs to be before
-rules = defaultdict(list)
+rules = defaultdict(set)
 updates = []
 update_section = False
 
@@ -21,14 +21,16 @@ with open(dir_path + "/input.txt") as file:
             split_line = line.split("|")
             update_page = int(split_line[0].strip())
             rule_page = int(split_line[1].strip())
-            rules[update_page].append(rule_page)
+            rules[update_page].add(rule_page)
         # Parse page updates
         else:
             update = [int(x) for x in line.strip().split(",")]
             updates.append(update)
 
 # print(rules, updates)
+# Part 1
 middle_page_total = 0
+incorrect_updates = []
 for update in updates:
     rule_broken = False
     for page_inx, page in enumerate(update):
@@ -45,5 +47,26 @@ for update in updates:
     if not rule_broken:
         # Add middle value to total
         middle_page_total += update[len(update) // 2]
+    else:
+        incorrect_updates.append(update)
 
-print(f"Part 1: {middle_page_total=}")
+print(f"Part 1: {middle_page_total=}")  # 5732
+
+# Part 2
+part_2_middle_page_total = 0
+for update in incorrect_updates:
+    # Get all applicable rules with only relevant pages
+    applicable_rules = {page: set(update).intersection(rules[page]) for page in update}
+
+    rule_count = {}
+    for key, vals in applicable_rules.items():
+        rule_count[key] = len(vals)
+
+    correct_update = [
+        x[0] for x in sorted(rule_count.items(), key=lambda item: item[1])
+    ]
+
+    part_2_middle_page_total += correct_update[len(correct_update) // 2]
+
+
+print(f"Part 2: {part_2_middle_page_total=}")  # 4716
